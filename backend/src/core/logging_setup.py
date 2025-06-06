7a4e622758303fab7e944a87521abf7c946e9485
romakot321 <play62342@gmail.com>
2025-06-06 19:31:15 +0300

    Setup logging for fastapi

--- backend/src/core/logging_setup.py
+++ backend/src/core/logging_setup.py
@@ -1,10 +1,62 @@
+from fastapi import FastAPI, Request, Response
 from loguru import logger
+import loguru
+from starlette.background import BackgroundTask
 from src.core.config import settings
 
 
-def add_custom_fields(record):
+def add_app_name(record):
     record["extra"]["app_name"] = settings.PROJECT_NAME
     return True
 
 
-logger.add("/app/logs/app.json", format="{time:MMMM D, YYYY > HH:mm:ss!UTC} | {level} | {message}", serialize=True, rotation="500 MB", filter=add_custom_fields)
+def add_http_info(request, response):
+    info = {
+        "req": {
+            "url": request.url.path,
+            "headers": request.headers,
+            "method": request.method,
+        },
+        "res": {
+            "status_code": response.status_code,
+        },
+    }
+
+    def patcher(record) -> None:
+        record["extra"]["http"] = info
+
+    return patcher
+
+
+def setup_fastapi_logging(app: FastAPI):
+    fastapi_logger = loguru.Logger()
+    fastapi_logger = fastapi_logger.patch(add_app_name)
+    fastapi_logger.add(
+        "/app/logs/fastapi.json",
+        format="{time:MMMM D, YYYY > HH:mm:ss!UTC} | {level} | {message}",
+        serialize=True,
+        rotation="30 MB",
+    )
+
+    def log(request: Request, response: Response):
+        level = "DEBUG"
+        if response.status_code >= 400:
+            level = "INFO"
+        elif response.status_code >= 500:
+            level = "ERROR"
+        fastapi_logger.patch(add_http_info(request, response)).log(level, request.method + " " + request.url.path)
+
+    @app.middleware("http")
+    async def log_request(request: Request, call_next):
+        response: Response = await call_next(request)
+        response.background = BackgroundTask(log, request, response)
+        return response
+
+
+logger.add(
+    "/app/logs/app.json",
+    format="{time:MMMM D, YYYY > HH:mm:ss!UTC} | {level} | {message}",
+    serialize=True,
+    rotation="30 MB",
+    filter=add_app_name,
+)
--- backend/src/main.py
+++ backend/src/main.py
@@ -3,8 +3,11 @@ from fastapi import FastAPI
 from src.integration.api.rest import router as integration_router
 from src.task.api.rest import router as task_router
 import src.core.logging_setup
+from src.core.logging_setup import setup_fastapi_logging
 
 app = FastAPI(title="Song Cover API")
 
 app.include_router(task_router, tags=["Task"], prefix="/api/task")
 app.include_router(integration_router, tags=["Integration"], prefix="/api/integration")
+
+setup_fastapi_logging(app)
